9e27c37ab90ec3835af3d7f5734cd8e99a42f638
nazmul162001 <nazmulprime162001@gmail.com>
2022-09-23 16:10:07 +0600

    added models & controller

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 
-
 const app = require("./app");
 
 // database connection
@@ -14,7 +13,6 @@ mongoose
   .then(() => {
     console.log(`Database connection is successful 🛢`.red.bold);
   });
-
 
 // server
 const port = process.env.PORT || 8000;
