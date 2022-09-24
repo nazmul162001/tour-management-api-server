77125df73262a7b82a209ee4231ec799a6cff942
nazmul162001 <nazmulprime162001@gmail.com>
2022-09-24 20:09:16 +0600

    fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose
   });
 
 // server
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
